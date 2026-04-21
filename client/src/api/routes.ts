3bec2030b77b853526edff456b9a31aab8039455
Jayant443 <jayanttodmal@gmail.com>
2026-04-22 01:31:22 +0530

    add env var base url in client

--- client/src/api/routes.ts
+++ client/src/api/routes.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://127.0.0.1:8000`;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 export const userRoute = `${BASE_URL}/users`;
 export const authRoute = `${BASE_URL}/auth`;
 export const communityRoute = `${BASE_URL}/r`;
